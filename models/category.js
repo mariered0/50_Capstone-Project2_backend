08c9e680667513930b8260cd6178bd7af9db5714
mariered0 <marieakg@gmail.com>
2023-04-10 21:19:31 -0700

    add category methods and routes

--- models/category.js
+++ models/category.js
@@ -11,7 +11,7 @@ class Category {
      *  Returns { items: [{ itemName, itemDesc, itemPrice }, ...]}
      */
 
-    static async findItems(category){
+    static async get(category){
         const result = await db.query(
                 `SELECT item_name AS "itemName",
                         item_desc AS "itemDesc",
@@ -39,6 +39,61 @@ class Category {
         );
         const categories = result.rows;
         return categories
+    }
+
+    /** Create a new category 
+     * 
+     *  Returns { created: category_name }
+     */
+
+    static async create(categoryName){
+        const duplicateCheck = await db.query(
+            `SELECT category_name
+             FROM categories
+             WHERE category_name = $1`,
+            [categoryName]);
+
+        if (duplicateCheck.rows[0]) throw new BadRequestError(`Duplicate category: ${categoryName}`);
+
+        const result = await db.query(
+            `INSERT INTO categories
+             (category_name)
+             VALUES ($1)
+             RETURNING category_name`,
+             [categoryName]);
+
+        const newCategory = result.rows[0];
+        
+        return newCategory;
+    }
+
+    /** Update a category
+     * 
+     *  Returns 
+     */
+
+    static async update(categoryName){
+        console.log(categoryName)
+        const category = await db.query(`
+            SELECT category_name
+            FROM categories
+            WHERE category_name = $1`,
+            [categoryName]);
+
+        console.log('category', category);
+        
+        if(category.rows[0]) throw new BadRequestError(`Duplicate category: ${categoryName}`);
+
+        const result = await db.query(`
+            INSERT INTO categories
+            (category_name)
+            VALUES ($1)
+            RETURNING category_name`,
+            [categoryName]);
+
+        const updatedCategory = result.rows[0];
+
+        return updatedCategory;
     }
 }
 
--- routes/categories.js
+++ routes/categories.js
@@ -5,9 +5,13 @@
 const jsonschema = require('jsonschema');
 const express = require('express');
 
+const { BadRequestError } = require('../expressError');
+const { ensureAdmin } = require('../middleware/auth');
 const Category = require('../models/category');
 
-const router = express.Router();
+const categoryNewSchema = require('../schemas/categoryNew.json');
+
+const router = new express.Router();
 
 
 /** GET /:category => { items: [{ itemName, itemDesc, itemPrice }, ...]}
@@ -17,11 +21,68 @@ const router = express.Router();
 
 router.get('/:category', async function (req, res, next) {
     try{
-        const items = await Category.findItems(req.params.category);
+        const items = await Category.get(req.params.category);
         return res.json({ items });
     }catch(err){
         return next(err);
     }
 });
+
+/** GET / => { categories: Cat1, Cat2, ... }
+ * 
+ * Returns a list of categories.
+ */
+
+router.get('/', async (req, res, next) => {
+    try{
+        const categories = await Category.findAll();
+        return res.json({ categories });
+    }catch(err){
+        return next(err);
+    }
+})
+
+/** POST / => { category_name: category }
+ * 
+ *  Returns a category created.
+ */
+router.post('/', ensureAdmin, async (req, res, next) => {
+    try{
+        const validator = jsonschema.validate(req.body, categoryNewSchema);
+        if(!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const newCategory = await Category.create(req.body.categoryName);
+        return res.status(201).json(newCategory);
+    }catch(err){
+        return next(err);
+    }
+})
+
+/** PATCH /:categoryName => { category_name: category }
+ *  
+ * Returns category updated
+ */
+
+router.patch('/:category', ensureAdmin, async (req, res, next) => {
+    try{
+        const validator = jsonschema.validate(req.body, categoryNewSchema);
+        if(!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const category = await Category.update(req.body.categoryName);
+        return res.json(category);
+    } catch(err){
+        return next(err);
+    }
+});
+
+
+
+
 
 module.exports = router;

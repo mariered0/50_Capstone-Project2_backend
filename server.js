94772f47b9076e76cfb837045e0f41d506c998c7
mariered0 <marieakg@gmail.com>
2023-05-20 19:43:04 -0700

    remove 'cors'

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 "use strict";
 
 const app = require("./app");
-const cors = require('cors');
 const { PORT } = require("./config");
-
-app.use(cors({
-    origin: "*"
-}))
 
 app.listen(PORT, function () {
     console.log(`Started on http://localhost:${PORT}`);
